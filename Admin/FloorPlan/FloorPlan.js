f61c9ce4eaf5006b509e578b7d4cf32643db6443
Laurie B <42420692+wtf-laurie@users.noreply.github.com>
2019-04-29 10:23:44 -1000

    Add files via upload

--- Admin/FloorPlan/FloorPlan.js
+++ Admin/FloorPlan/FloorPlan.js
@@ -11,4 +11,27 @@ window.onclick = function(event) {
   }
 }
 
-    
+
+// Get the modal
+/* var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+    var colors = [rgb(129, 247, 129), rgb(247, 129, 129)];
+
+    var colorIndex = 0;
+  function changeColor() {
+        var col = document.getElementById("body");
+        if( colorIndex >= colors.length ) {
+            colorIndex = 0;
+        }
+        col.style.backgroundColor = colors[colorIndex];
+        colorIndex++;
+    } */
+
+var color = [rgb(129, 247, 129), rgb(247, 129, 129)];
+var i = 0;
+document.querySelector("button").addEventListener("click"),
+  function(){
+    i = i < color.length ? ++i : 0;
+document.querySelector("body").style.background =  color[i]
+  }
